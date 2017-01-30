68bdc3a3b53c7b40051b8b6a111661bcd07e7763
apple <chgpgf@gamil.com>
2017-01-30 12:37:10 +0800

    implement crud

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,9 +8,45 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(groups_params)
+    if @group.save
+      redirect_to groups_path
+      flash[:notice] = "Created"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+    if @group.update(groups_params)
+      flash[:notice] = "Updated"
+      redirect_to groups_path
+    else
+      redner :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "Deleted"
+    redirect_to groups_path
+  end
+
   private
 
   def groups_params
-    params.require(:groups).permit(:title, :description)
+    params.require(:group).permit(:title, :description)
   end
 end
